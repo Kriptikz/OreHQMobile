e3bc667fae356b13c29ce0c1a496aa094af183b3
Kriptikz <Kriptikz@users.noreply.github.com>
2024-10-07 18:16:24 -0500

    NewWalletScreen styling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,12 +67,12 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
+    debugImplementation(libs.ui.tooling)
 //    testImplementation(libs.junit)
 //    androidTestImplementation(libs.androidx.junit)
 //    androidTestImplementation(libs.androidx.espresso.core)
 //    androidTestImplementation(platform(libs.androidx.compose.bom))
 //    androidTestImplementation(libs.androidx.ui.test.junit4)
-//    debugImplementation(libs.androidx.ui.tooling)
 //    debugImplementation(libs.androidx.ui.test.manifest)
 
     // Java Native Access for UniFFI library
--- app/src/main/java/com/kriptikz/orehqmobile/ui/screens/new_wallet_start_screen/NewWalletStartScreen.kt
+++ app/src/main/java/com/kriptikz/orehqmobile/ui/screens/new_wallet_start_screen/NewWalletStartScreen.kt
@@ -1,5 +1,6 @@
 package com.kriptikz.orehqmobile.ui.screens.new_wallet_start_screen
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,8 +12,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.kriptikz.orehqmobile.ui.screens.OreHQMobileScaffold
+import com.kriptikz.orehqmobile.ui.screens.home_screen.HomeScreen
+import com.kriptikz.orehqmobile.ui.screens.home_screen.HomeUiState
+import com.kriptikz.orehqmobile.ui.theme.OreHQMobileTheme
 
 @Composable
 fun NewWalletScreen(
@@ -27,9 +34,16 @@ fun NewWalletScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Looks like there is no Mining Hot Wallet for this app yet. Get a pen & paper and click generate to get started!",
+                text = "Looks like there is no Mining Hot Wallet for this app yet.",
                 style = MaterialTheme.typography.headlineSmall,
-                modifier = Modifier.padding(bottom = 16.dp)
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 4.dp)
+            )
+            Text(
+                text = "Get a pen & paper and click generate to get started!",
+                style = MaterialTheme.typography.headlineSmall,
+                textAlign = TextAlign.Center,
+                modifier = Modifier.padding(bottom = 24.dp)
             )
             Button(
                 onClick = onButtonGenerate,
@@ -38,5 +52,15 @@ fun NewWalletScreen(
                 Text("Generate")
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewWalletScreenPreview() {
+    OreHQMobileTheme {
+        NewWalletScreen(
+            onButtonGenerate = {}
+        )
     }
 }
